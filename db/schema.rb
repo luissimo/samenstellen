5ad230730d5903486fe86e76ea5602cca7471450
luissimo <khijbar@gmail.com>
2017-03-18 17:40:23 +0100

    models and controllers set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170318163548) do
+
+  create_table "boxsprings", force: :cascade do |t|
+    t.string   "onderstel"
+    t.string   "hoofdbord"
+    t.string   "voetbord"
+    t.string   "stijl"
+    t.string   "kleur"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "customize_id"
+  end
+
+  create_table "customizes", force: :cascade do |t|
+    t.string   "gender"
+    t.integer  "age"
+    t.string   "email"
+    t.string   "weight"
+    t.integer  "length"
+    t.string   "sleep_position"
+    t.string   "body_shape"
+    t.string   "warm_sleeping"
+    t.boolean  "body_complaints"
+    t.string   "diseases"
+    t.string   "size"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "name"
+  end
 
 end

f93c531625ebea36c82a97900c931f7f1a00d917
luissimo <khijbar@gmail.com>
2017-11-22 22:53:11 +0100

    add schema to category question

--- app/assets/javascripts/home/script.js
+++ app/assets/javascripts/home/script.js
@@ -1,11 +1,3 @@
-// preloader
-$(window).on('load', function(){
-    $("body").css("overflow","auto");
-    $(".preloader").fadeOut(200,function(){
-        $(this).remove();
-    });
-});
-
 // show question 2 if clicked on 2 persoonsmatras | homepage
 $(".home-question-2").hide()
 $(".show-home-question-2").on('click', function() {

9a6001d1d37c5e189ef74f2703d4e71992544bc5
luissimo <khijbar@gmail.com>
2017-12-03 17:07:15 +0100

    add question explanation popovers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     Time.now.year
   end
 
+  def question_icon_sm(text, data_placement: "top")
+    image_tag('question_icon.png', alt: 'vraag?', class: 'question_label_icon_sm', "data-toggle" => "popover", "data-placement" => data_placement, title: " ", "data-content" => text, "data-trigger" => "focus", tabindex: 0 )
+  end
+
+  def question_icon_md(text, data_placement: "bottom")
+    image_tag('question_icon.png', alt: 'vraag?', class: 'question_label_icon_md', "data-toggle" => "popover", "data-placement" => data_placement, title: " ", "data-content" => text, "data-trigger" => "focus", tabindex: 0 )
+  end
+
 end

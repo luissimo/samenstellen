149d0bad57bafec255325137f8249c9dbd79dbbe
luissimo <khijbar@gmail.com>
2018-01-13 15:36:40 +0100

    precompile assets

--- public/assets/custom-82633ae5c3292295e0f1a7029bd2d713660b88151ef3d0f0b451b3e243c33331.js
+++ public/assets/custom-82633ae5c3292295e0f1a7029bd2d713660b88151ef3d0f0b451b3e243c33331.js
@@ -23,41 +23,41 @@ $('.name_person_2').change(function() {
    $('.show_name_person_2').text( $('.name_person_2').val() );
 });
 
-// Show diseases on fields on click checkbox && show/display category question
-$(".diseases_options").hide();
-$('.show_diseases').click(function() {
-    if( this.checked ) {
-        $(".diseases_options").show();
-    } else {
-        $(".diseases_options").hide();
-    };
-    // Hide and disable budget and comfort categories on diseases
-    if( this.checked || $('.show_diseases_2').checked ) {
-      $(".disable_on_diseases").prop("disabled", true);
-      $(".disable_on_diseases").hide();
-    } else {
-      $(".disable_on_diseases").prop("disabled", false);
-      $(".disable_on_diseases").show();
-    }
-});
+// // Show diseases on fields on click checkbox && show/display category question
+// $(".diseases_options").hide();
+// $('.show_diseases').click(function() {
+//     if( this.checked ) {
+//         $(".diseases_options").show();
+//     } else {
+//         $(".diseases_options").hide();
+//     };
+//     // Hide and disable budget and comfort categories on diseases
+//     // if( this.checked || $('.show_diseases_2').checked ) {
+//     //   $(".disable_on_diseases").prop("disabled", true);
+//     //   $(".disable_on_diseases").hide();
+//     // } else {
+//     //   $(".disable_on_diseases").prop("disabled", false);
+//     //   $(".disable_on_diseases").show();
+//     // }
+// });
 
-// Show diseases2 on fields on click checkbox && show/display category question
-$(".diseases_options_2").hide();
-$('.show_diseases_2').click(function() {
-    if( this.checked ) {
-        $(".diseases_options_2").show();
-    } else {
-        $(".diseases_options_2").hide();
-    };
-    // Hide and disable budget and comfort categories on diseases
-    if( this.checked || $('.show_diseases').checked ) {
-      $(".disable_on_diseases").prop("disabled", true);
-      $(".disable_on_diseases").hide();
-    } else {
-      $(".disable_on_diseases").prop("disabled", false);
-      $(".disable_on_diseases").show();
-    }
-});
+// // Show diseases2 on fields on click checkbox && show/display category question
+// $(".diseases_options_2").hide();
+// $('.show_diseases_2').click(function() {
+//     if( this.checked ) {
+//         $(".diseases_options_2").show();
+//     } else {
+//         $(".diseases_options_2").hide();
+//     };
+//     // Hide and disable budget and comfort categories on diseases
+//     // if( this.checked || $('.show_diseases').checked ) {
+//     //   $(".disable_on_diseases").prop("disabled", true);
+//     //   $(".disable_on_diseases").hide();
+//     // } else {
+//     //   $(".disable_on_diseases").prop("disabled", false);
+//     //   $(".disable_on_diseases").show();
+//     // }
+// });
 
 /////// CHECKOUT PAGE ////////
 
@@ -101,15 +101,6 @@ $(".last_field_address_information").keyup(function() {
   }
 });
 
-$("#floor_options").hide();
-$('#floor_select').click(function() {
-    if( !this.checked ) {
-      $("#floor_options").show();
-    } else {
-      $("#floor_options").hide();
-    }
-});
-
 // show alert text if specific answer is chosen
 $("#chassis_chosen").hide();
 $(".show_chassis_chosen_warning").on('ifChanged', function() {
@@ -120,14 +111,7 @@ $(".show_chassis_chosen_warning").on('ifChanged', function() {
   }
 });
 
-
 // INITIALIZE TOOLTIPS
 $(document).ready(function(){
   $('[data-toggle="popover"]').popover();
-});
-
-
-// remove cookie notice on click outside of notice
-$("body").on('click', function() {
-  $('.ckcontent').hide();
 });

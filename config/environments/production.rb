6682b820bdc73e96a7ecfd18edeac36d65b62fc2
luissimo <khijbar@gmail.com>
2018-02-14 00:58:11 +0100

    change config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'tenzen.nl' }
+  config.action_mailer.default_url_options = { host: 'https://www.tenzen.nl' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:             'smtp.gmail.com',
       port:                 587,
-      domain:               'tenzen.nl',
+      domain:               'gmail.com',
       authentication:       :plain,
       user_name:            ENV['GMAIL_USERNAME_DEV'],
       password:             ENV['GMAIL_PASSWORD_DEV']

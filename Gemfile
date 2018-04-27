b549f8bf0c2775f2becd4b8aac8bfd28f8172268
luissimo <khijbar@gmail.com>
2018-04-27 18:49:50 +0200

    update rails 5.1.2 and add heroku sitemap configs

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.4.1'
 
-gem 'rails', '5.1.1'
+gem 'rails', '5.1.2'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
@@ -16,7 +16,8 @@ gem 'dotenv-rails'
 gem 'mail_form'
 gem 'jbuilder', '~> 2.5'
 gem 'sitemap_generator'
-gem 'whenever'
+gem 'fog-aws'
+gem 'whenever', require: false
 gem 'activerecord-session_store'
 
 group :development, :test do
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,8 +1,18 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.tenzen.nl"
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+  fogprovider: 'AWS',
+  awsaccesskeyid: ENV['AWSACCESSKEYID'],
+  awssecretaccesskey: ENV['AWSSECRETACCESSKEY'],
+  fogdirectory: ENV['S3BUCKET'],
+  fogregion: ENV['AWS_REGION'])
 
-# create index for when multiple sitemaps
-SitemapGenerator::Sitemap.create_index = true
+SitemapGenerator::Sitemap.sitemapshost = "https://s3-#{ENV['AWSREGION']}.amazonaws.com/#{ENV['S3_BUCKET']}/"
+SitemapGenerator::Sitemap.sitemapspath = 'sitemaps/'
+SitemapGenerator::Sitemap.publicpath = 'tmp/'
+
+# # create index for when multiple sitemaps
+# SitemapGenerator::Sitemap.create_index = true
 
 SitemapGenerator::Sitemap.create do
   # Usage: add(path, options={})

17369281f64dac991a6e083b9ed82a7d3e5d9ea9
luissimo <khijbar@gmail.com>
2018-05-25 13:01:00 +0200

    shuffle images seo

--- app/helpers/seo_pages_helper.rb
+++ app/helpers/seo_pages_helper.rb
@@ -4,7 +4,7 @@ module SeoPagesHelper
   end
 
   def image_seo_block_2
-    image_tag 'koud_matras.jpg', alt: "Matras kopen #{@city_name}", title: "Matras kopen #{@city_name}", width: '100%', height: '100%'
+    image_tag 'Tenzen-matras-in-doos-Blauw.png', alt: "Matras kopen #{@city_name}", title: "Matras kopen #{@city_name}", width: '100%', height: '100%'
   end
 
   def image_seo_block_3

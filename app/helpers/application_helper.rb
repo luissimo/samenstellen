7c8b49867575089e1c31f767de7530438794a8f1
luissimo <khijbar@gmail.com>
2018-09-09 18:04:58 +0200

    fix question popover on firefox

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,14 @@ module ApplicationHelper
   end
 
   def question_icon_sm(text, data_placement: "top")
-    image_tag('question_icon.png', alt: 'vraag?', class: 'question_label_icon_sm', "data-toggle" => "popover", "data-placement" => data_placement, title: " ", "data-content" => text, "data-trigger" => "focus", tabindex: 0 )
+    # FIXME needs to be updated like question_icon_md to work on firefox before using
+    image_tag('question_icon.png', alt: 'Vraag?', class: 'question_label_icon_sm', "data-toggle" => "popover", "data-placement" => data_placement, title: " ", "data-content" => text, "data-trigger" => "focus", role: 'button', tabindex: 0 )
   end
 
   def question_icon_md(text, data_placement: "bottom")
-    image_tag('question_icon.png', alt: 'vraag?', class: 'question_label_icon_md', "data-toggle" => "popover", "data-placement" => data_placement, title: " ", "data-content" => text, "data-trigger" => "focus", tabindex: 0 )
+    content_tag(:a, "data-toggle" => "popover", "data-placement" => data_placement, "data-content" => text, "data-trigger" => "focus", role: 'button', tabindex: 0) do
+      image_tag('question_icon.png', alt: 'Vraag?',class: "question_label_icon_md", title: "Vraag?")
+    end
   end
 
   def alert(message, warning = true, warning_message = t("general.beware"), id: nil)

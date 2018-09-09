3f87c09cf829b7dcea2804e9ec93b7ed8eae1107
luissimo <khijbar@gmail.com>
2018-09-09 19:06:39 +0200

    remove unused css and js

--- app/assets/javascripts/home/script.js
+++
@@ -1,6 +0,0 @@
-// show question 2 if clicked on 2 persoonsmatras | homepage
-// $(".home-question-2").hide()
-// $(".show-home-question-2").on('click', function() {
-//     $(".home-question-1").hide();
-//     $(".home-question-2").show();
-// })
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,17 +12,14 @@ Rails.application.config.assets.version = '1.0'
 
 ## JAVASCRIPTS && STYLESHEETS
 Rails.application.config.assets.precompile += %w( custom.js jquery.validate.js jquery.placeholder.js
-																									jquery.bxslider.min.js quantity-bt.js
-																									bootstrap.js owl.carousel.min.js functions.js fancy_func.js
-																									check_radio/jquery.icheck.js modernizr.custom.17475.js respond.min.js
-																									jquery.wizard.js jquery-ui-1.8.22.min.js jquery-1.10.2.min.js
-																									fancybox/source/jquery.fancybox.pack.js fancybox/source/helpers/jquery.fancybox-media.js
+																									bootstrap.js functions.js check_radio/jquery.icheck.js
+                                                  modernizr.custom.17475.js respond.min.js jquery.wizard.js
+                                                  jquery-ui-1.8.22.min.js jquery-1.10.2.min.js
 
-                                                  home/jquery.min.js home/bootstrap.js home/jquery.counterup.min.js home/owl.carousel.min.js
-                                                  home/owl.carousel.min.js home/jquery.easing.min.js home/script.js
+                                                  home/jquery.min.js home/bootstrap.js home/jquery.counterup.min.js
+                                                  home/jquery.easing.min.js
 
-																									bootstrap.css style.css fancybox/source/jquery.fancybox.css
-																									check_radio/skins/square/aero.css jquery.switch.css owl.carousel.css owl.theme.css
-
-                                                  home/bootstrap.css home/font-awesome.min.css home/flaticon.css home/owl.carousel.min.css home/colors/color-1.css
-                                                  home/style.css)
+																									bootstrap.css style.css check_radio/skins/square/aero.css jquery.switch.css
+                                                  home/bootstrap.css home/font-awesome.min.css home/flaticon.css home/colors/color-1.css
+                                                  home/style.css
+                                                  )
